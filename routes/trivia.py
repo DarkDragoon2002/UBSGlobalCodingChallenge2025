4d5eca656de7242ab34b808a64806f5d7df98c9c
DarkDragoon2002 <deepankrishnaa2002@gmail.com>
2025-09-06 11:24:02 +0800

    Update trivia.py

--- routes/trivia.py
+++ routes/trivia.py
@@ -3,11 +3,10 @@ import logging
 
 from routes import app
 
+logger = logging.getLogger(__name__)
+
 @app.route('/trivia', methods=['GET'])
 def evaluate():
     result = {"answers": [4, 1, 2, 2, 3, 4, 4, 5, 4]}
     logging.info("My result :{}".format(result))
     return jsonify(result)
-
-if __name__ == "__main__":
-    app.run(debug=True)
